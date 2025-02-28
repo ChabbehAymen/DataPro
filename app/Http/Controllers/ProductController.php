f62d67798b3ba32bb549f677e3fab1fc248c99fd
ChabbehAymen <devaymen15.5.3@gmail.com>
2025-02-28 00:23:05 +0000

    completed crud for product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\ProductService;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function __construct(protected ProductService $service)
+    {}
+
+    public function index()
+    {
+        return $this->service->all();
+    }
+
+    public function show($id)
+    {
+        return $this->service->find($id);
+    }
+
+    public function store(Request $request)
+    {
+        if($this->service->create($request,
+            ["title"=>"required|string|max:255",
+                "desc"=>"required|string|max:255",
+                "price"=>"required|integer"]))
+            return redirect(route('product.index'));
+        else return redirect(route('product.create'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $this->service->update($id, $request);
+        return redirect(route('product.index'));
+    }
+
+    public function destroy($id)
+    {
+        $this->service->delete($id);
+        return redirect(route('product.index'));
+    }
 }
---
+++ app/Services/ProductService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services;
+
+use App\Repository\ProductRepository;
+use App\Repository\RepositoryContract;
+
+class ProductService extends BaseService
+{
+    public function __construct(ProductRepository $repository)
+    {
+        parent::__construct($repository);
+    }
+}
