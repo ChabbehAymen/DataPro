ced93ae77611733890e8956c18cb7206d90e7264
Aymen Chabbeh <91035373+ChabbehAymen@users.noreply.github.com>
2025-03-01 00:57:11 +0000

    Update BasketRepository.php

--- app/Repository/BasketRepository.php
+++ app/Repository/BasketRepository.php
@@ -6,7 +6,8 @@ use App\Models\Basket;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Collection;
 
-class BasketRepository{
+class BasketRepository extends BaseRepository
+{
 
     public function __construct(Basket $model){
         parent::__construct($model);
