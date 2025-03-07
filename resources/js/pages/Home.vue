3bf7e2a7271f9d80a0bf1307f0c24db85859f48d
ChabbehAymen <devaymen15.5.3@gmail.com>
2025-03-07 11:39:48 +0000

    fixed errors

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -7,9 +7,7 @@
 </template>
 
 <script setup>
-
 import Header from "../components/Header.vue";
 import Product from "../components/Product.vue";
-
 </script>
 
--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,8 @@
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/{vue_capture}', function () {
+   return view('welcome');
+})->where('vue_capture', "[\/\w\.-]*")->name('vue');
+
 Route::get('/', [ProductController::class, 'index'])->name('index');
