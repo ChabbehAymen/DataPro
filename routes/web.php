291b78a8b6f44123b755f93b84075a62f0ec8a93
Dina Dehbi <dinadh012@gmail.com>
2025-03-10 19:15:52 +0000

    CRUD Tags

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\TagController;
 
 Route::resource('tags', TagController::class);
 
+Route::get('/', [ProductController::class, 'index'])->name('index');
 
 
 
@@ -15,7 +16,6 @@ Route::get('/{vue_capture}', function () {
    return view('welcome');
 })->where('vue_capture', "[\/\w\.-]*")->name('vue');
 
-Route::get('/', [ProductController::class, 'index'])->name('index');
 
 
 
