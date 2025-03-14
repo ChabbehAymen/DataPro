be468921b261e2c442f549e1f6dfd73f2642005f
ZOUBAIRDouae <zoubairdouae5@gmail.com>
2025-03-14 10:49:26 +0000

    Carousal

---
+++ resources/js/components/Carousel.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from 'vue';
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination, Autoplay } from 'swiper/modules';
+
+const slides = ref([
+    { id: 1, image: '/images/arm-site-img.png'},
+    { id: 2, image: '/images/Armoire.jpg'},
+    { id: 3, image: '/images/images2.jpg'}
+]);
+</script>
+
+<template>
+  <swiper
+    :modules="[Pagination, Autoplay]"  
+    :slides-per-view="1"
+    :space-between="10"
+    :pagination="{ clickable: true }"
+    :autoplay="{ delay: 3000 }"
+    class="w-[300px] h-[300px]" 
+  >
+    <swiper-slide v-for="slide in slides" :key="slide.id">
+      <div class="relative w-full h-full">
+        <img :src="slide.image" alt="Carousel Image" class="w-full h-full object-cover">
+      </div>
+    </swiper-slide>
+  </swiper>
+</template>
+
+<style scoped>
+/* Removed styles for arrows */
+img {
+    max-height: 300px;
+    object-fit: cover;
+}
+</style>
--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -3,14 +3,21 @@
 */
 <template>
     <Header/>
-    <div class=" w-full mx-auto my-11 flex flex-wrap gap-5">
-        <Product v-for="product in products" :id="product.id" :title="product.title" :desc="product.desc" :price="product.price" :category="product.category"/>
-    </div>
+    <div class="relative w-full min-h-screen"> 
+    <!-- Fix the Carousel in the Top-Right Corner -->
+        <div class="absolute top-0 right-0 w-[500px] h-[300px] z-10">
+            <Carousel />
+        </div>
+        <div class=" w-full mx-auto my-11 flex flex-wrap gap-5">
+            <Product v-for="product in products" :id="product.id" :title="product.title" :desc="product.desc" :price="product.price" :category="product.category"/>
+        </div>
+    </div>   
 </template>
 
 <script setup>
 import Header from "../components/Header.vue";
 import Product from "../components/Product.vue";
+import Carousel from "../components/Carousel.vue";
 import {ref} from "vue";
 const products = ref([]);
 try {
